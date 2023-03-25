5d9a1f01e7fdb4e4db95d5b62b2483aaf60d409d
Ifiok Udoidiok <ifiok@fleetrover.com>
2023-03-25 20:06:42 +0100

    Implement methods to get all tests to pass

--- src/LOTRClass.js
+++ src/LOTRClass.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+import LOTRClassError from "./LOTRClassError.js";
+import * as constants from "./constants.js";
 
 export default class LOTRClass {
   constructor(token = null) {
@@ -7,16 +8,89 @@ export default class LOTRClass {
   }
 
   async getAllMovies() {
- 
-  }
+    let config = {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    };
 
+    let response = await axios
+      .get(`${constants.BASE_URL}${constants.MOVIE_BASE_ENDPOINT}`, config)
+      .catch((error) => {
+        if (error.response) {
+          throw new LOTRClassError(
+            error.response.status,
+            error.response.data.message
+              ? error.response.data.message
+              : error.response.data
+          );
+        } else {
+          throw new LOTRClassError("ERROR", error.message);
+        }
+      });
+
+    return { movies: response.data.docs, total: response.data.total };
+  }
 
   async getMovieById(id) {
- 
+    let config = {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    };
+
+    let response = await axios
+      .get(
+        `${constants.BASE_URL}${constants.MOVIE_BASE_ENDPOINT}/${id}`,
+        config
+      )
+      .catch((error) => {
+        if (error.response) {
+          throw new LOTRClassError(
+            error.response.status,
+            error.response.data.message
+              ? error.response.data.message
+              : error.response.data
+          );
+        } else {
+          throw new LOTRClassError("ERROR", error.message);
+        }
+      });
+
+    return response.data;
   }
 
-
   async getQuotesByMovieId(id) {
-  
+    let config = {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    };
+
+    let response = await axios
+      .get(
+        `${constants.BASE_URL}${constants.MOVIE_BASE_ENDPOINT}/${id}${constants.QUOTE_BASE_ENDPOINT}`,
+        config
+      )
+      .catch((error) => {
+        if (error.response) {
+          throw new LOTRClassError(
+            error.response.status,
+            error.response.data.message
+              ? error.response.data.message
+              : error.response.data
+          );
+        } else {
+          throw new LOTRClassError("ERROR", error.message);
+        }
+      });
+
+    return {
+      quotes: response.data.docs,
+      limit: response.data.limit,
+      page: response.data.page,
+      pages: response.data.pages,
+      total: response.data.total,
+    };
   }
 }
