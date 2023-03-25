58377fd082a5195cd6124f40e2a9052e5c0c4cf6
Ifiok Udoidiok <ifiok@fleetrover.com>
2023-03-25 20:02:39 +0100

    Define class with methods: - getAllMovies - getMovieById - getQuotesByMovieId

---
+++ src/LOTRClass.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+
+export default class LOTRClass {
+  constructor(token = null) {
+    this.token = token;
+  }
+
+  async getAllMovies() {
+ 
+  }
+
+
+  async getMovieById(id) {
+ 
+  }
+
+
+  async getQuotesByMovieId(id) {
+  
+  }
+}
